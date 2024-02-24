882c09b598668ae16aedff3bb817c8b44ea651bb
Miles Mena <menamil28@outlook.com>
2024-02-24 13:19:38 -0700

    add evaluate_validation func

--- train.py
+++ train.py
@@ -17,6 +17,9 @@ import torch.optim as optim
 import matplotlib.pyplot as plt
 from itertools import islice
 import json
+import csv
+
+
 # I despise np arrays that display in scientific notaton
 np.set_printoptions(formatter={'float_kind':'{:f}'.format})
 
@@ -33,15 +36,16 @@ def main():
     # Install the most stable version of pytorch with GPU configuration:  https://pytorch.org/get-started/locally/
 
     ############## Hyperparameters #################
-    RESIZE = 128
-    BATCH_SIZE = 4 # I hear you are supposed to use as much GPU as possible, but batch size affects the loss propagations. Look into this tradeoff 
-    EPOCHS = 1
-    LR = .001
+    RESIZE = 1024
+    BATCH_SIZE = 2 # I hear you are supposed to use as much GPU as possible, but batch size affects the loss propagations. Look into this tradeoff 
+    EPOCHS = 100
+    LR = .0001
     # use GPU
     DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
     # For Ryan's puny mac:
     DEVICE = 'mps' if torch.backends.mps.is_available() else DEVICE
     print(DEVICE)
+    evaluate_in_loop = False
 
     # Set the random seed for CPU operations
     torch.manual_seed(42)
@@ -51,7 +55,7 @@ def main():
 
     LOSS_WEIGHT = torch.ones(3).to(DEVICE) # 3 is the number of classes for this task
     # calculate weights by processing dataset histogram balancing by class 
-    LOSS_WEIGHT[0], LOSS_WEIGHT[1], LOSS_WEIGHT[2] = .1,2,100                                # CLASS LABELS: {0:soil, 1:crop, 2: weed}
+    LOSS_WEIGHT[0], LOSS_WEIGHT[1], LOSS_WEIGHT[2] = (1/88.45), (1/11.03), (1/.5)                          # CLASS LABELS: {0:soil, 1:crop, 2: weed}
 
     ############# Init the Phenobench DataLoader #################
     # Phenobench's DataLoader sits on top the directroy and only loads when .__getitem__ is called
@@ -101,7 +105,7 @@ def main():
     # Define the Adam optimizer
     optimizer = optim.Adam(model.parameters(), lr = LR)
 
-
+    flag = False
     ############### Train the Model ###############
     eval_loss = []
     epoch_evals = []
@@ -110,33 +114,93 @@ def main():
     for i in range(EPOCHS):
         batch_loss = []
         for batch in tqdm(train_loader): # # testing code: tqdm(islice(train_loader,8))  Training model:  tqdm(train_loader)
-        
+            model.train()
+            optimizer.zero_grad()  # Zero the gradients, so we only back prop this batch not this batch and all of the batches before it
+
             ############ Compute Loss for each Batch ###########
             with autocast(): # context manager. AMP. Uses float16 and float32 when appropriate to decrease computations expense 
                 loss = loss_func(model(batch['images'].to(DEVICE)), batch['masks'].long().to(DEVICE))
                 loss = loss.requires_grad_()
                 batch_loss.append(loss)
             ############ Backpropagate the Loss ##############
-            optimizer.zero_grad()  # Zero the gradients, so we only back prop this batch not this batch and all of the batches before it
-            loss.backward()  # Compute gradients
-            optimizer.step()  # Update parameters
+            # if loss.item() == float('nan'):
+            #     mean_val_loss, mean_val_iou = evaluate_validation(model, val_loader, DEVICE, loss_func)
+            #     break
+            
+            scaler.scale(loss).backward()  # Compute gradients
+            scaler.step(optimizer) # Update parameters
+            scaler.update()
+
+        
 
         print(f'Epoch {i} mean loss: {torch.mean(torch.stack(batch_loss))}')
 
+        if (i + 1) % 5 == 0: 
+            mean_val_loss, mean_val_iou = evaluate_validation(model, val_loader, DEVICE, loss_func)
+                
+            model_parameters = {
+                        "batch_size": BATCH_SIZE,
+                        "epochs": EPOCHS,
+                        "learning_rate": LR,
+                        "loss_weight": LOSS_WEIGHT.tolist(),
+                        "resize": RESIZE,
+                        "device": DEVICE,
+                        "mean_val_loss": mean_val_loss,
+                        "mean_val_iou": mean_val_iou.tolist()
+                    }
+                    
+            save_model(model, model_parameters)
 
-        # convert batch loss into np array on the CPU
-        batch_loss = torch.stack(batch_loss).detach().cpu().numpy()
-        epoch_loss.append(batch_loss)
+        ################## MODEL STATISTICS #############
+        # # convert batch loss into np array on the CPU
+        # batch_loss = torch.stack(batch_loss).detach().cpu().numpy()
+        # epoch_loss.append(batch_loss)
+        # if flag:
+        #     break
+        # ############### Evaluate on Validation ###########
+        # # I am unsure if we should be evaluating or applying the loss function
+        # # Evaluation easily shows the performance on all of the class, but I think generally people look at the validation loss
+        # # be careful to avoid overfitting on the validation set
+        # if evaluate_in_loop: 
+        #     mean_val_loss, mean_val_iou = evaluate_validation(model, val_loader, DEVICE, loss_func)
+        #     print(f"Val Mean Loss: {mean_val_loss}, Validation IoU: {mean_val_iou}")
+        # #np.savetxt('validation_prediction.txt', pred.reshape(-1, 1).detach().cpu().numpy())
+    
+    
 
-     
-            
-        ############### Evaluate on Validation ###########
-        # I am unsure if we should be evaluating or applying the loss function
-        # Evaluation easily shows the performance on all of the class, but I think generally people look at the validation loss
-        # be careful to avoid overfitting on the validation set
+    if not evaluate_in_loop:
+        mean_val_loss, mean_val_iou = evaluate_validation(model, val_loader, DEVICE, loss_func)
+        print(f"Val Mean Loss: {mean_val_loss}, Validation IoU: {mean_val_iou}")
+
+    # TODO: We need to save the model, but I also think we should save the results and the hyperparameters by appending to a csv file: training_loss, val_loss, training_time, training_time per batch, IoU, etc
+    model_parameters = {
+        "batch_size": BATCH_SIZE,
+        "epochs": EPOCHS,
+        "learning_rate": LR,
+        "loss_weight": LOSS_WEIGHT.tolist(),
+        "resize": RESIZE,
+        "device": DEVICE,
+        "mean_val_loss": mean_val_loss,
+        "mean_val_iou": mean_val_iou.tolist()
+    }
+    
+    save_model(model, model_parameters)
+
+def save_model(model, model_stats={}):
+    model_id = str(np.random.randint(10000))
+    model_path = os.path.join("models", model_id)
+    os.makedirs(model_path)
+    torch.save(model, os.path.join(model_path, "model.pt"))
+    with open(os.path.join(model_path, "model_stats.json"), "w") as json_file:
+        json.dump(model_stats, json_file, indent=4)
+    print(f"Model saved at {model_path}")
+
+def evaluate_validation(model, val_loader, DEVICE, loss_func):
+        model.eval()
         multi_jaccard = MulticlassJaccardIndex(num_classes=3, average = None).to(DEVICE)
         val_loss = []
         val_iou = []
+        print('VALIDATION: ')
         for batch in tqdm(val_loader): # testing code: tqdm(islice(train_loader),8)  Training model:  tqdm(val_loader)
             
             with autocast(): # context manager. AMP. 
@@ -150,76 +214,8 @@ def main():
         mean_val_loss = np.array(val_loss).mean()
         mean_val_iou = np.array(val_iou).mean(axis = 0) # mean of the columns
 
-        
-        print(f"Val Mean Loss: {mean_val_loss}, Validation IoU: {mean_val_iou}")
-        #np.savetxt('validation_prediction.txt', pred.reshape(-1, 1).detach().cpu().numpy())
+        return mean_val_loss, mean_val_iou
 
 
-    #    if plot_batch_loss:
-    #         ax1.plot(batch_loss, label = f'Epoch {i}')
-    #     # Add title and axis labels
-    # fig1, ax1 = plt.subplots()
-    # if plot_batch_loss:
-    #     ax1.set_title('Batch Losses CE weight = (%s,%s,%s)'%(w1,w2,w3))
-    #     ax1.set_xlabel('Batch Index')
-    #     ax1.set_ylabel('Loss')
-    #     ax1.legend()
-    #     fig1.savefig("images/single_batch_losses_weight(%s,%s,%s).png"%(w1,w2,w3))
-
-    # #print(epoch_evals)
-    # #epoch_loss = epoch_loss.detach().cpu().numpy()
-    # #print(epoch_loss)
-    # fig2, ax2 = plt.subplots()
-    # epoch_loss_flat = [loss for epoch_list in epoch_loss for loss in epoch_list]
-    # ax2.plot(epoch_loss_flat)
-    # #ax2.plot(epoch_loss[1], label = 'Epoch 1')
-    # # Add title and axis labels
-    # ax2.set_title('Batch Losses CE weight = (%s,%s,%s)'%(w1,w2,w3))
-    # ax2.set_xlabel('Batch Index')
-    # ax2.set_ylabel('Loss')
-    # #ax2.legend()
-    # fig2.savefig("images/epochs_batch_losses_weight(%s,%s,%s).png"%(w1,w2,w3))
-
-    # #print(eval_loss)
-    # # I'd like to plot the evaluation loss for each epochand the training loss for each epoch
-    # epoch_evals = np.array([[tensor.detach().cpu().numpy() for tensor in eval_list] for eval_list in epoch_evals])
-    
-    # fig3, ax3 = plt.subplots()
-    # # iterate over the second axis
-    # for i in range(epoch_evals.shape[1]):
-
-    #     ax3.plot(epoch_evals[:,i], label = 'JaccardIndex')
-    # ax3.plot(np.mean(np.array(epoch_loss), axis = 1), label = 'Loss')
-    # #plt.show(fig3)
-    # ax3.set_title('validation and loss by epoch weight(%s,%s,%s)'%(w1,w2,w3))
-    # ax3.set_xlabel('Epoch Index')
-    # ax3.set_ylabel('Loss and JaccardIndex')
-    # ax3.legend()
-    # fig3.savefig("images/eval_loss_by_epoch_weight(%s,%s,%s).png"%(w1,w2,w3))
-        
-    # TODO: We need to save the model, but I also think we should save the results and the hyperparameters by appending to a csv file: training_loss, val_loss, training_time, training_time per batch, IoU, etc
-    model_parameters = {
-        "batch_size": BATCH_SIZE,
-        "epochs": EPOCHS,
-        "learning_rate": LR,
-        "loss_weight": LOSS_WEIGHT.tolist(),
-        "resize": RESIZE,
-        "device": DEVICE,
-        "mean_val_loss": mean_val_loss,
-        "mean_val_iou": mean_val_iou.tolist()
-    }
-    save_model(model, model_parameters)
-
-
-def save_model(model, model_stats={}):
-    model_id = str(np.random.randint(10000))
-    model_path = os.path.join("models", model_id)
-    os.makedirs(model_path)
-    torch.save(model, os.path.join(model_path, "model.pt"))
-    with open(os.path.join(model_path, "model_stats.json"), "w") as json_file:
-        json.dump(model_stats, json_file, indent=4)
-    print(f"Model saved at {model_path}")
-
-                
 if __name__ == "__main__":
     main()
